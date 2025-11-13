2e214e1b0f644b7dd931e59c3c368974c3ae203b
Joshua Lester <jlester3@ewu.edu>
2025-11-13 15:52:13 -0800

    Initial commit

---
+++ ConsoleApp2/ConsoleApp2/FunWithLinq.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2;
+
+public class FunWithLinq
+{
+    public static void Test()
+    {
+        
+        
+    }
+}
