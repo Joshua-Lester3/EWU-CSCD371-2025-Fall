6c40a6c75203c82be65c98f142edd759f1b14e3a
Joshua Lester <jlester3@ewu.edu>
2025-11-24 08:47:36 -0800

    init commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp1;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Task dotsTask = Task.Run(() => {
+            Console.WriteLine("...");
+            throw new InvalidOperationException("Exception!!");
+        });
+
+        Task dashesTask = Task.Run(() =>
+        {
+            Console.WriteLine("---");
+        });
+        Console.WriteLine("Hello, World!");
+
+        //dashesTask.Wait();
+
+        //Thread.Sleep(1000000000);
+        //Task.Delay(10000);
+
+        dotsTask.Wait();
+
+        List<int> numbers = new() { 1, 2, 3, 4, 5 };
+
+        //dotsTask.Dispose();
+    }
+}
